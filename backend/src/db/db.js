05129d3664e72dea2aa52cd35f5309f34e1f65fa
shubh165 <shubhendratiwari165@gmail.com>
2026-07-26 19:52:24 +0530

    refactor: simplify MongoDB connection log message

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    console.log(`MongoDB connected !! DB HOST: ${connection.connection.host}`);
+    console.log(`MongoDB connected`);
   } catch (error) {
     console.error('2.Error connecting to MongoDB:',error);
     process.exit(1);
